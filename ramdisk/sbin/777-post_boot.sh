ccbe698d83d036f2d3b5ad138e5353aca97e00d6
777jon <jcruzdeveloper@gmail.com>
2015-08-23 22:21:05 -0700

    post_boot: tune default kernel settings
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>

--- ramdisk/sbin/777-post_boot.sh
+++ ramdisk/sbin/777-post_boot.sh
@@ -4,6 +4,7 @@ BB=/sbin/bb/busybox
 
 ############################
 # Custom Kernel Settings for 777 Kernel!
+# Based on Render Script by RenderBroken
 #
 echo "[777-Kernel] Boot Script Started" | tee /dev/kmsg
 stop mpdecision
@@ -13,10 +14,10 @@ stop mpdecision
 #
 echo 1 > /sys/module/msm_hotplug/min_cpus_online
 echo 2 > /sys/module/msm_hotplug/cpus_boosted
-echo 1000 > /sys/module/msm_hotplug/down_lock_duration
+echo 500 > /sys/module/msm_hotplug/down_lock_duration
 echo 2500 > /sys/module/msm_hotplug/boost_lock_duration
-echo 10 > /sys/module/msm_hotplug/update_rates
-echo 99 > /sys/module/msm_hotplug/fast_lane_load
+echo 200 5:100 50:50 350:200 > /sys/module/msm_hotplug/update_rates
+echo 100 > /sys/module/msm_hotplug/fast_lane_load
 echo 1 > /sys/module/msm_hotplug/max_cpus_online_susp
 
 ############################
@@ -39,15 +40,53 @@ echo 0 > /sys/module/msm_thermal/core_control/enabled
 echo 1 > /sys/module/msm_thermal/parameters/enabled
 
 ############################
+# CPU-Boost Settings
+#
+echo 20 > /sys/module/cpu_boost/parameters/boost_ms
+echo 500 > /sys/module/cpu_boost/parameters/input_boost_ms
+echo 0:1497600 1:1497600 2:1497600 3:1497600 > /sys/module/cpu_boost/parameters/input_boost_freq
+echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold
+echo 1 > /sys/module/cpu_boost/parameters/hotplug_boost
+echo 1 > /sys/module/cpu_boost/parameters/wakeup_boost
+
+############################
+# Tweak Background Writeout
+#
+echo 200 > /proc/sys/vm/dirty_expire_centisecs
+echo 40 > /proc/sys/vm/dirty_ratio
+echo 5 > /proc/sys/vm/dirty_background_ratio
+echo 10 > /proc/sys/vm/swappiness
+
+############################
+# Power Effecient Workqueues (Enable for battery)
+#
+echo 1 > /sys/module/workqueue/parameters/power_efficient
+
+############################
 # Scheduler and Read Ahead
 #
 echo zen > /sys/block/mmcblk0/queue/scheduler
 echo 1024 > /sys/block/mmcblk0/bdi/read_ahead_kb
 
 ############################
-# Sweep2Sleep
+# Governor Tunings
 #
-echo 2 > /sys/android_touch/sweep2wake
+echo interactive > /sys/kernel/msm_limiter/scaling_governor_0
+echo 20000 1400000:40000 1700000:20000 > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
+echo 90 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
+echo 1190400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
+echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
+echo 85 1500000:90 1800000:70 > /sys/devices/system/cpu/cpufreq/interactive/target_loads
+echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
+echo 30000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
+echo 100000 > /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis
+echo 30000 > /sys/devices/system/cpu/cpufreq/interactive/timer_slack
+
+############################
+# LMK Tweaks
+#
+echo 2560,4096,8192,16384,24576,32768 > /sys/module/lowmemorykiller/parameters/minfree
+echo 32 > /sys/module/lowmemorykiller/parameters/cost
 
 ############################
 # Synapse + UKM Support
